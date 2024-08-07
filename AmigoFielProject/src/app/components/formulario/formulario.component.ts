34afd29da8d54f8ad178cca34d99b2e947ae8dc3
Thyale MD <thyaledantas@gmail.com>
2024-08-07 11:14:44 -0300

    Formulário de Adoção finalizado

--- AmigoFielProject/src/app/components/formulario/formulario.component.ts
+++ AmigoFielProject/src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-formulario',
@@ -6,5 +7,118 @@ import { Component } from '@angular/core';
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  nome: string = '';
+  idade: number | null = null;
+  endereco: string = '';
+  telefone: string = '';
+  email: string = '';
+
+  tipoResidencia: string = '';
+  residenciaPropria: string = '';
+  permissaoAnimais: string = '';
+  espacoDisponivel: string = '';
+  telasProtecao: string = '';
+
+  numeroPessoas: number | null = null;
+  concordancia: string = '';
+  alergias: string = '';
+
+  jaTeveAnimais: string = '';
+  possuiOutrosAnimais: string = '';
+
+  motivoAdocao: string = '';
+  tempoSozinho: string = '';
+
+  visitaRepresentante: string = '';
+  termoResponsabilidade: string = '';
+
+  declaracao: string = '';
+  termosConcordancia: string = '';
+
+  DadosAdote = {
+    nome: this.nome,
+    idade: this.idade,
+    endereco: this.endereco,
+    telefone: this.telefone,
+    email: this.email,
+    tipoResidencia: this.tipoResidencia,
+    residenciaPropria: this.residenciaPropria,
+    permissaoAnimais: this.permissaoAnimais,
+    espacoDisponivel: this.espacoDisponivel,
+    telasProtecao: this.telasProtecao,
+    numeroPessoas: this.numeroPessoas,
+    concordancia: this.concordancia,
+    alergias: this.alergias,
+    jaTeveAnimais: this.jaTeveAnimais,
+    possuiOutrosAnimais: this.possuiOutrosAnimais,
+    motivoAdocao: this.motivoAdocao,
+    tempoSozinho: this.tempoSozinho,
+    visitaRepresentante: this.visitaRepresentante,
+    termoResponsabilidade: this.termoResponsabilidade,
+    declaracao: this.declaracao,
+    termosConcordancia: this.termosConcordancia
+  };
+
+  EnviarForm() {
+    if (
+      this.nome === '' ||
+      this.idade === null ||
+      this.endereco === '' ||
+      this.telefone === '' ||
+      this.email === '' ||
+      this.tipoResidencia === '' ||
+      this.residenciaPropria === '' ||
+      this.permissaoAnimais === '' ||
+      this.espacoDisponivel === '' ||
+      this.telasProtecao === '' ||
+      this.numeroPessoas === null ||
+      this.concordancia === '' ||
+      this.alergias === '' ||
+      this.jaTeveAnimais === '' ||
+      this.possuiOutrosAnimais === '' ||
+      this.motivoAdocao === '' ||
+      this.tempoSozinho === '' ||
+      this.visitaRepresentante === '' ||
+      this.termoResponsabilidade === '' ||
+      this.declaracao === '' ||
+      this.termosConcordancia === ''
+    ) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Erro!',
+        text: 'Por favor, preencha todos os campos.',
+      });
+    } else {
+      Swal.fire({
+        icon: 'success',
+        title: 'Formulário enviado com sucesso!',
+        text: 'Estamos felizes que você decidiu adotar um novo amigo, vamos analisar seu perfil e em breve entraremos em contato!',
+      });
+      this.DadosAdote = {
+        nome: this.nome,
+        idade: this.idade,
+        endereco: this.endereco,
+        telefone: this.telefone,
+        email: this.email,
+        tipoResidencia: this.tipoResidencia,
+        residenciaPropria: this.residenciaPropria,
+        permissaoAnimais: this.permissaoAnimais,
+        espacoDisponivel: this.espacoDisponivel,
+        telasProtecao: this.telasProtecao,
+        numeroPessoas: this.numeroPessoas,
+        concordancia: this.concordancia,
+        alergias: this.alergias,
+        jaTeveAnimais: this.jaTeveAnimais,
+        possuiOutrosAnimais: this.possuiOutrosAnimais,
+        motivoAdocao: this.motivoAdocao,
+        tempoSozinho: this.tempoSozinho,
+        visitaRepresentante: this.visitaRepresentante,
+        termoResponsabilidade: this.termoResponsabilidade,
+        declaracao: this.declaracao,
+        termosConcordancia: this.termosConcordancia
+      };
+      localStorage.setItem('FormAdotante', JSON.stringify(this.DadosAdote))
+    }
+  }
 
 }
