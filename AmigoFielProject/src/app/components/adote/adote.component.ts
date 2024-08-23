c5abf8d6213c7760e205e38c6f0f211370e60554
Thyale MD <thyaledantas@gmail.com>
2024-08-23 11:02:36 -0300

    Nomes e img dos pets

--- AmigoFielProject/src/app/components/adote/adote.component.ts
+++ AmigoFielProject/src/app/components/adote/adote.component.ts
@@ -10,14 +10,14 @@ export class AdoteComponent {
   tranparencia:boolean = false
   ShowDadosPets:boolean = false
 
-  imgPets = [
-    "CatAdult.png",
-    "CatAdult2.png",
-    "CatAdult3.png",
-    "CatBaby.png",
-    "DogAdult.png",
-    "DogBaby.png",
-    "DogBeagleAdult.png"
+  Pets = [
+    {nome:"Renecca", img: "CatAdult.png"},
+    {nome:"Cadu", img:"CatAdult2.png" },
+    {nome:"Marie", img:"CatAdult3.png" },
+    {nome:"Figma", img:"CatBaby.png" },
+    {nome: "Snoopy", img: "DogAdult.png"},
+    {nome: "Yanna", img : "DogBaby.png"},
+    {nome:"Zeca", img: "DogBeagleAdult.png"}
   ]
 
   AbrirDados(){
